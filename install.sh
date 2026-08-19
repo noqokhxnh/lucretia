1211f53ff31e2a726cae924fd94fc3d9cd4ed022
noqokhxnh <khanh2k5xxx@gmail.com>
2026-08-19 10:36:55 +0700

    increments dot_versions

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 # set -e
 
 # Script Versioning & Initialization
-DOTS_VERSION="1.4.2"
+DOTS_VERSION="1.4.3"
 VERSION_FILE="$HOME/.local/state/lucretia-version"
 
 # Terminal UI Colors & Formatting
